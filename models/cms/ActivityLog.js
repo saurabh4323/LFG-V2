14ae2ca5fdf9deaba08820c7e78c0c709cc9be40
saurabh singh <138216611+saurabh4323@users.noreply.github.com>
2026-06-16 15:19:05 +0530

    Update ActivityLog.js

--- models/cms/ActivityLog.js
+++ models/cms/ActivityLog.js
@@ -1,1 +1,37 @@
 
+import mongoose from 'mongoose';
+
+const ActivityLogSchema = new mongoose.Schema({
+  clientId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'CMS_Client',
+    required: true,
+    index: true
+  },
+  businessId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Business',
+    required: true
+  },
+  type: {
+    type: String,
+    enum: ['Status Change', 'Comment', 'File Upload', 'Task Update', 'Service Created', 'Auto Event'],
+    required: true
+  },
+  action: { type: String, required: true },
+  details: mongoose.Schema.Types.Mixed,
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  isVisibleToClient: {
+    type: Boolean,
+    default: false
+  },
+  metadata: Map
+}, {
+  timestamps: true,
+  collection: 'cms_activity_logs'
+});
+
+export default mongoose.models.CMS_ActivityLog || mongoose.model('CMS_ActivityLog', ActivityLogSchema);
